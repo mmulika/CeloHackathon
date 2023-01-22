f9369babbd552fc9a93f7b883ea7ba4014055005
mmulika <mulikamike@gmail.com>
2023-01-22 09:03:30 +0300

    polkadotwallet

---
+++ polkadot/index.js
@@ -0,0 +1,27 @@
+const { ApiPromise, WsProvider } = require('@polkadot/api');
+const { Keyring } = require('@polkadot/api');
+
+
+
+
+const createAccount = async (req, res)=>{
+const provider = new WsProvider('wss://rpc.polkadot.io');
+const api = await ApiPromise.create({ provider});
+const keyring = new Keyring({ type: 'sr25519' });
+const newAccount = keyring.addFromUri('//Alice');
+console.log(newAccount.address);
+
+}
+
+
+const checkbalance = async (req,res)=>{
+const provider = new WsProvider('wss://rpc.polkadot.io');
+const api = await ApiPromise.create({ provider});
+const address = '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY';
+const balance = await api.query.balances.freeBalance(address);
+console.log(balance.toString());
+
+}
+
+createAccount()
+checkbalance()
